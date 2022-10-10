75af94048792f3d3b576b02cf9cbf27398484633
HydroJiang <hydrojiang@qq.com>
2022-10-10 22:33:47 +0800

    testing

---
+++ testfile.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=0;
+    int b=1;
+    int c=a+b;
+    for(int i=0;i<10;i++)
+        c++;
+    cout<<"hello world: "<<c<<endl;
+}
